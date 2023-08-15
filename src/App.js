febc71f0e5d78221bb69b4d0e82a9f8857e1dcc1
ASEEM GUPTA <iamgeekyaseem@gmail.com>
2023-08-15 17:34:01 +0530

    Add cards using bulma.io css library

--- src/App.js
+++ src/App.js
@@ -1,16 +1,44 @@
+import 'bulma/css/bulma.css'
 import ProfileCard from './ProfileCard';
 import AlexaImg from './images/alexa.png'
 import CortanaImg from './images/cortana.png'
 import SiriImg from './images/siri.png'
 
-function App(){
-    return(
-       <div>
-        <div>Personal Digital Assistants</div>
-        <ProfileCard title='Alexa' handle='@alexa99' image={AlexaImg} />
-        <ProfileCard title='Cortana' handle='@cortana69' image={CortanaImg} />
-        <ProfileCard title='Siri' handle='@siri83' image={SiriImg} />
-       </div>
+function App() {
+    return (
+        <div>
+            <section className='hero is-primary'>
+                <div className='hero-body'>
+                    <p className='title'>
+                    Personal Digital Assistant
+                    </p>
+                </div>
+            </section>
+        
+            <div className='container'>
+                <section className='section'>
+                    <div className='columns'>
+                        <div className='column is-3'>
+                            <ProfileCard title='Alexa'
+                            handle='@alexa99'
+                            image={AlexaImg}
+                            description="Adipisicing deserunt labore mollit Lorem incididunt duis pariatur nisi est officia."
+                            />
+                        </div>
+                        <div className='column is-3'>
+                            <ProfileCard title='Cortana' handle='@cortana69' image={CortanaImg}
+                            description="Nulla deserunt do pariatur aliquip duis ex laborum amet. Nostrud elit aute dolore proident aliqua pariatur veniam occaecat."
+                            />
+                        </div>
+                        <div className='column is-3'>
+                            <ProfileCard title='Siri' handle='@siri83' image={SiriImg}
+                            description="Sit dolore qui tempor occaecat nulla culpa nisi. Eiusmod nulla mollit consectetur sint eiusmod adipisicing enim ad do nisi aute velit sint non."
+                            />
+                        </div>
+                    </div>
+                </section>
+            </div>
+        </div>
     )
 }
 
